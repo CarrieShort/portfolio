22213be830b5f0f484f69689e6462b16ec1b02b2
CarrieShort <carrie.r.phillips@gmail.com>
2016-03-07 19:43:17 -0800

    add placeholder data to portfolioProjects

---
+++ scripts/portfolioProjects.js
@@ -0,0 +1,18 @@
+var portfolioData = [
+  {
+    title:       'Bacon Ipsum',
+    previewImage:   'http://www.imdb.com/name/nm0000102/',
+    shortDesc: 'I show up on hover',
+    publishedOn: '2015-11-05',
+    detailedLink: 'portfolio/projecturl.html',
+    description:        '<p>Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p><p><img src="http://baconmockup.com/600/300/" class="pull-right" > Cupidatat pancetta chuck fugiat shankle dolor drumstick excepteur spare ribs duis. Meatloaf beef ribs et picanha eu in qui dolore tongue enim spare ribs capicola est pork chop swine. Ipsum strip steak nostrud ham hock qui irure. <img src="http://baconmockup.com/300/400/" class="pull-right">Picanha occaecat prosciutto, rump tempor jerky proident voluptate short loin jowl.</p>'
+  },
+  {
+    title:       'Bacon Ipsum',
+    previewImage:   'http://www.imdb.com/name/nm0000102/',
+    shortDesc: 'I show up on hover',
+    publishedOn: '2015-11-05',
+    detailedLink: 'portfolio/projecturl.html',
+    description:        '<p>Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p><p><img src="http://baconmockup.com/600/300/" class="pull-right" > Cupidatat pancetta chuck fugiat shankle dolor drumstick excepteur spare ribs duis. Meatloaf beef ribs et picanha eu in qui dolore tongue enim spare ribs capicola est pork chop swine. Ipsum strip steak nostrud ham hock qui irure. <img src="http://baconmockup.com/300/400/" class="pull-right">Picanha occaecat prosciutto, rump tempor jerky proident voluptate short loin jowl.</p>'
+  }
+];
