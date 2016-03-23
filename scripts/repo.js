46c4851f4e733b495970bf556595ec5751317f80
CarrieShort <carrie.r.phillips@gmail.com>
2016-03-23 14:32:26 -0700

    delete commented out code

--- scripts/repo.js
+++ scripts/repo.js
@@ -18,26 +18,6 @@
       });
       callback(filteredRepos);
     });
-
-    // console.log('repo fired');
-    // $.ajax({
-    //   url: 'https://api.github.com/repos/carrieshort/' + repo + '/stats/contributors',
-    //   type: 'GET',
-    //   headers: {
-    //     'Authorization': 'token ' + githubToken
-    //   },
-    //   success: function(data, message, xhr) {
-    //     reposStats.all = data;
-    //     var filteredRepos = reposStats.all.map(function(ele){
-    //       var userStats = {};
-    //       userStats.name = ele.author.login;
-    //       userStats.url = ele.author.html_url;
-    //       userStats.commits = ele.total;
-    //       return userStats;
-    //     });
-    //     callback(filteredRepos);
-    //   }
-    // });
   };
 
   reposStats.with = function(attr) {
