54956744a78df1343764669c236993f1dab5c4bc
CarrieShort <carrie.r.phillips@gmail.com>
2016-03-23 14:10:10 -0700

    refactor requestRepo to work with a hidden resource

--- scripts/repo.js
+++ scripts/repo.js
@@ -4,25 +4,40 @@
   reposStats.all = [];
 
   reposStats.requestRepos = function(repo,callback) {
-    console.log('repo fired');
-    $.ajax({
-      url: 'https://api.github.com/repos/carrieshort/' + repo + '/stats/contributors',
-      type: 'GET',
-      headers: {
-        'Authorization': 'token ' + githubToken
-      },
-      success: function(data, message, xhr) {
-        reposStats.all = data;
-        var filteredRepos = reposStats.all.map(function(ele){
-          var userStats = {};
-          userStats.name = ele.author.login;
-          userStats.url = ele.author.html_url;
-          userStats.commits = ele.total;
-          return userStats;
-        });
-        callback(filteredRepos);
-      }
+    $.get('github/repos/carrieshort/' +
+            repo +
+            '/stats/contributors')
+    .done(function(data,message,xhr){
+      reposStats.all = data;
+      var filteredRepos = reposStats.all.map(function(ele){
+        var userStats = {};
+        userStats.name = ele.author.login;
+        userStats.url = ele.author.html_url;
+        userStats.commits = ele.total;
+        return userStats;
+      });
+      callback(filteredRepos);
     });
+
+    // console.log('repo fired');
+    // $.ajax({
+    //   url: 'https://api.github.com/repos/carrieshort/' + repo + '/stats/contributors',
+    //   type: 'GET',
+    //   headers: {
+    //     'Authorization': 'token ' + githubToken
+    //   },
+    //   success: function(data, message, xhr) {
+    //     reposStats.all = data;
+    //     var filteredRepos = reposStats.all.map(function(ele){
+    //       var userStats = {};
+    //       userStats.name = ele.author.login;
+    //       userStats.url = ele.author.html_url;
+    //       userStats.commits = ele.total;
+    //       return userStats;
+    //     });
+    //     callback(filteredRepos);
+    //   }
+    // });
   };
 
   reposStats.with = function(attr) {
